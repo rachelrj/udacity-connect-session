078f1a42b5193b1fea42e503230b27ef5c9ac645
rjoyce <rjoyce@splunk.com>
2022-11-08 21:52:03 -0700

    Display images in images folder. Add duck image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+let listOfImages = [];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  importAll(r) {
+    return r.keys().map(r);
+  }
+  componentWillMount() {
+    listOfImages = this.importAll(
+      require.context("./images/", false, /\.(png|jpe?g|svg)$/)
+    );
+  }
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+          <div style={{ margin: "25px" }}>
+            {listOfImages.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt='info'
+                style={{ width: "500px" }}
+              ></img>
+            ))}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
